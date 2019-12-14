85d1aaf69c898c084f6a3cd7d754d68faf6b7172
BCG12345 <30796091+BCG12345@users.noreply.github.com>
2019-12-14 16:19:27 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,13 +9,15 @@ html = scraperwiki.scrape("https://advocates.sindhbarcouncil.org/enrollments_lis
 #
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
+
+print(root)
 #root.cssselect("div[align='left']")
 #
 # # Write out to the sqlite database using scraperwiki library
 #scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
 # # An arbitrary query against the database
-scraperwiki.sql.select("* from data where 'name'='peter'")
+#scraperwiki.sql.select("* from data where 'name'='peter'")
 
 
 
